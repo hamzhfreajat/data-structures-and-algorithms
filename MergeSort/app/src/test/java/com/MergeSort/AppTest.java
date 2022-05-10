d545e513457b876c2c101b3f83e290d8b3b7437e
Hamzh Freijat <hamzhg2016@gmail.com>
2022-05-10 11:41:41 +0300

    merge sort update

--- MergeSort/app/src/test/java/com/MergeSort/AppTest.java
+++ MergeSort/app/src/test/java/com/MergeSort/AppTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
-    public void insertSortTest(){
+    public void mergeSortTest(){
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(8);
         arrayList.add(4);
